223779141b9cf814a7668bc3aa9d411f04e8302b
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-04-09 19:47:58 +0530

    learning server actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function getUserDetails() {
+  try {
+    const user = await client.user.findFirst({});
+	  return {
+      name: user?.username,
+      email: user?.username
+    }
+  }  catch(e) {
+    console.log(e);
+  }
+}  
 
 export default async function Home() {
-
-  
+  const userData = await getUserDetails();
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
--- components/signup.tsx
+++ components/signup.tsx
@@ -1,5 +1,5 @@
 "use client"
-import axios from "axios";
+import { signup } from "@/app/actions/user";
 import { useRouter } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
 
@@ -25,10 +25,7 @@ export function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
                         <button onClick={async () => {
-                            const response = await axios.post("http://localhost:3000/api/user", {
-                                username,
-                                password
-                            });
+                        const res = await signup(username,password)
                             router.push("/")
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
