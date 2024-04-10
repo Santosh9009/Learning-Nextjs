1147cf691600869fe20e12f5f3c82fbb40db70a9
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-04-10 23:30:05 +0530

    fetching data in next

--- app/page.tsx
+++ app/page.tsx
@@ -19,15 +19,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userData?.name}
-                </div>
-                
-                {userData?.email}
-            </div>
-        </div>
+    
     </div>
   );
 }
